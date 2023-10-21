6c8aa0609e728e772705f36a78120b35e73a31c6
arefin22 <shamsularefin500@gmail.com>
2023-10-21 15:41:51 +0600

    Updated Routes

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ async function run() {
 
     app.delete('/cart/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: (id) }
+      console.log(id);
+      const query = { _id: new ObjectId(id) }
       const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
